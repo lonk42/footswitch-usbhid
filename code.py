66d6f0089e3e854db2f5e17e5c9f8270c2905356
Lyndon <lyndon@onthelink.net>
2023-05-02 20:16:19 +1200

    Added debounce

--- code.py
+++ code.py
@@ -22,9 +22,9 @@ class HID_Device():
     self.keyboard = Keyboard(usb_hid.devices)
 
     # Setup switches
-    self.footswitches = {'a': {'pin': digitalio.DigitalInOut(board.GP13), 'last_state': -1, 'keycode': Keycode.A}}
+    self.footswitches = {'a': {'pin': digitalio.DigitalInOut(board.GP13), 'last_state': -1, 'last_trigger': time.monotonic(), 'keycode': Keycode.F20}}
     for key, value in self.footswitches.items():
-      value['pin'].switch_to_input(pull=digitalio.Pull.DOWN)
+      value['pin'].switch_to_input(pull=digitalio.Pull.UP)
 
     # Watch the buttons
     self.switch_loop()
@@ -36,11 +36,16 @@ class HID_Device():
       # Check the switches
       for key, switch in self.footswitches.items():
         current_value = switch['pin'].value
+        #print(str(current_value))
 
         # Only trigger on press
-        if current_value == 1 and switch['last_state'] == 0 and switch['last_state'] != -1:
-          print("Footswitch '" + key + '" triggered!, sending "' + switch['keycode'] + '"...')
-          self.keyboard.send(switch['keycode'])
+        if current_value == 0 and switch['last_state'] == 1 and switch['last_state'] != -1:
+
+          # Debounce
+          if time.monotonic() - switch['last_trigger'] > 0.1:
+            print("Footswitch '" + key + '" triggered!, sending "' + str(switch['keycode']) + '"...')
+            self.keyboard.send(switch['keycode'])
+            switch['last_trigger'] = time.monotonic()
 
         switch['last_state'] = current_value
 
